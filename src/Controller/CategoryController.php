2115cef15ebaa76693ad15fb489068d7ec75348c
Nguyễn Tiến Đạt <129811133+tiendat3001@users.noreply.github.com>
2023-08-08 14:04:48 +0700

    omg

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,5 +17,20 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', [
             'categories'=>$categories
         ]);
+    }
+
+    #[Route('/category/delete/{id}', name: 'category_delete')]
+    public function deleteAction(ManagerRegistry $doctrine, $id): Response
+    {
+        $em = $doctrine->getManager();
+        $categories = $em->getRepository('App\Entity\Category')->find($id);
+        $em->remove($categories);
+        $em->flush();
+
+        $this->addFlash(
+            'error',
+            'Category deleted'
+        );
+        return $this->redirectToRoute('app_category');
     }
 }
